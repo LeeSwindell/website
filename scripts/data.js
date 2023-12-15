a0ecbccba02031e51be4405fef58f1a0fbe290bc
Lee Swindell <39568393+LeeSwindell@users.noreply.github.com>
2023-12-15 09:54:46 -0500

    text update

--- scripts/data.js
+++ scripts/data.js
@@ -2,45 +2,47 @@
 
 const data = [
   {
-    image: '../img/hogwartsbattle.png',
-    title: 'Hogwarts Battle',
-    time: '[Go/Python/React]',
-    description: 'Flawless board game design - absolute perfection',
-    link: 'https://github.com/leeswindell/boardgame-battle'
+    image: "../img/hogwartsbattle.png",
+    title: "Hogwarts Battle",
+    time: "[Go/React]",
+    description: "Multiplayer board game with Pub/Sub synchronization",
+    link: "https://github.com/leeswindell/boardgame-battle",
   },
   {
-    image: '../img/synth.png',
-    title: 'Synthesizer',
-    time: '[C++]',
-    description: 'Polyphonic Synthesizer built using C++, integrated with all major audio workstations.',
-    link: 'https://github.com/leeswindell/synth2'
+    image: "../img/synth.png",
+    title: "Synthesizer",
+    time: "[C++]",
+    description:
+      "Polyphonic Synthesizer built using C++, integrated with all major audio workstations.",
+    link: "https://github.com/leeswindell/synth2",
   },
   {
-    image: '../img/raft.png',
-    title: 'Raft Consensus Protocol',
-    time: '[Go]',
-    description: 'Implementation of the distributed systems research paper Raft using Go.',
-    link: 'https://github.com/leeswindell/Raft-6.824'
+    image: "../img/raft.png",
+    title: "Raft Consensus Protocol",
+    time: "[Go]",
+    description:
+      "Implementation of the distributed systems research paper Raft using Go.",
+    link: "https://github.com/leeswindell/Raft-6.824",
   },
   {
-    image: '../img/eq.png',
-    title: 'Equalizer',
-    time: '[C++]',
-    description: 'Audio equalizer built with C++ and the JUCE framework',
-    link: 'https://github.com/leeswindell/basicEQ'
+    image: "../img/eq.png",
+    title: "Equalizer",
+    time: "[C++]",
+    description: "Audio equalizer built with C++ and the JUCE framework",
+    link: "https://github.com/leeswindell/basicEQ",
   },
   {
-    image: '../img/record.png',
-    title: 'Lunar Tunes',
-    time: '[Java Spring/React]',
-    description: 'Audio streaming website made to learn design + animations',
-    link: 'https://github.com/leeswindell/lunar-tunes'
+    image: "../img/record.png",
+    title: "Lunar Tunes",
+    time: "[Java Spring/React]",
+    description: "Audio streaming website made to learn design + animations",
+    link: "https://github.com/leeswindell/lunar-tunes",
   },
   {
-    image: '../img/qm.png',
-    title: 'Quantum Labs',
-    time: '[Python]',
-    description: 'Numerical Analysis + modelling of modern QM problems',
-    link: 'https://github.com/leeswindell/example-QM'
+    image: "../img/qm.png",
+    title: "Quantum Labs",
+    time: "[Python]",
+    description: "Numerical Analysis + modelling of modern QM problems",
+    link: "https://github.com/leeswindell/example-QM",
   },
-]
+];
